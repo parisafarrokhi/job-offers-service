4528a149989773e081f6779eff8dfea0282e2cbf
Emily Henderson <emily.henderson@machine.ventures>
2025-03-02 03:25:47 +0330

    add swagger and job-offers documentaion

--- src/infrastructure/adapters/controllers/job.controller.ts
+++ src/infrastructure/adapters/controllers/job.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiTags, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { JobService } from '../../../application/services/job.service';
 
+@ApiTags('job-offers')
 @Controller('api')
 export class JobController {
-  constructor(private readonly jobService: JobService) {}
+  constructor(private readonly jobService: JobService) { }
 
   @Get()
   getHello(): string {
@@ -11,6 +13,44 @@ export class JobController {
   }
 
   @Get('/job-offers')
+  @ApiQuery({ name: 'page', required: false, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, example: 10 })
+  @ApiQuery({ name: 'location', required: false, example: 'San Francisco, CA' })
+  @ApiQuery({ name: 'jobId', required: false, example: 'P1-250' })
+  @ApiQuery({ name: 'title', required: false, example: 'Software Engineer' })
+  @ApiQuery({ name: 'compensationMin', required: false, example: 91000 })
+  @ApiQuery({ name: 'compensationMax', required: false, example: 136000 })
+  @ApiQuery({ name: 'employerName', required: false, example: 'DataWorks' })
+  @ApiQuery({ name: 'skills', required: false, example: 'Python,Machine Learning,SQL' })
+  @ApiResponse({
+    status: 200,
+    example: [
+      {
+        "_id": "67c393d03a016be404f848e2",
+        "jobId": "P1-250",
+        "title": "Software Engineer",
+        "location": "San Francisco, CA",
+        "compensation": {
+          "min": 91000,
+          "max": 136000,
+          "currency": "USD",
+          "_id": "67c393d03a016be404f848e3"
+        },
+        "employer": {
+          "companyName": "DataWorks",
+          "industry": "Design",
+          "_id": "67c393d03a016be404f848e4"
+        },
+        "skills": [
+          "Python",
+          "Machine Learning",
+          "SQL"
+        ],
+        "postedDate": "2025-02-22T16:15:49.465Z",
+        "__v": 0
+      }
+    ]
+  })
   async list(
     @Query('page') page = 1,
     @Query('limit') limit = 10,
@@ -32,7 +72,6 @@ export class JobController {
       skills: skills ? skills.split(',') : undefined,
     };
 
-    const jobs = await this.jobService.getOffers(page, limit, filters);
-    return jobs;
+    return await this.jobService.getOffers(page, limit, filters);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './infrastructure/config/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Offers API')
+    .setDescription('API documentation for job offers')
+    .addTag('job-offers')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
+  console.log(`Server running at http://localhost:3000`);
+  console.log(`Swagger Docs available at http://localhost:3000/api/docs`);
 }
 bootstrap();
